8f1ce16bad2d2c190fe4825de3d52d0698df7a58
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2024-06-04 11:50:35 +0200

    Nest tests into describe block

--- src/shape.test.ts
+++ src/shape.test.ts
@@ -1,14 +1,15 @@
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { getCubeGeometry } from "./shape";
 import { Vertex, Edge } from "./types";
 
-test("make sure a cube has the right vertices / edges", () => {
-  const vertices: Vertex[] = [];
-  const edges: Edge[] = [];
-  getCubeGeometry(vertices, edges, 1, 0, 0, 0);
+describe("test getCubeGeometry", () => {
+  test("make sure a cube has the right vertices / edges", () => {
+    const vertices: Vertex[] = [];
+    const edges: Edge[] = [];
+    getCubeGeometry(vertices, edges, 1, 0, 0, 0);
 
-  // prettier-ignore
-  const expectedVertices = [
+    // prettier-ignore
+    const expectedVertices = [
     [-1, -1, -1], // 0
     [ 1, -1, -1], // 1
     [ 1,  1, -1], // 2
@@ -18,71 +19,72 @@ test("make sure a cube has the right vertices / edges", () => {
     [ 1,  1,  1], // 6
     [-1,  1,  1], // 7
   ];
-  // prettier-ignore
-  const expectedEdges = [
+    // prettier-ignore
+    const expectedEdges = [
     [0, 1],   [3, 0],   [2, 1],   [2, 3], // Top face
     [4, 5],   [5, 6],   [6, 7],   [7, 4], // Bottom face
     [0, 4],   [1, 5],   [2, 6],   [3, 7], // Sides
   ];
 
-  expect(vertices).toEqual(expectedVertices);
-  expect(edges).toEqual(expectedEdges);
-});
+    expect(vertices).toEqual(expectedVertices);
+    expect(edges).toEqual(expectedEdges);
+  });
 
-test("make sure a cube with an offset has the right vertices / edges", () => {
-  const vertices: Vertex[] = [];
-  const edges: Edge[] = [];
-  getCubeGeometry(vertices, edges, 1, 1, 2, 3);
+  test("make sure a cube with an offset has the right vertices / edges", () => {
+    const vertices: Vertex[] = [];
+    const edges: Edge[] = [];
+    getCubeGeometry(vertices, edges, 1, 1, 2, 3);
 
-  const expectedVertices = [
-    [1, 3, 5],
-    [3, 3, 5],
-    [3, 5, 5],
-    [1, 5, 5],
-    [1, 3, 7],
-    [3, 3, 7],
-    [3, 5, 7],
-    [1, 5, 7],
-  ];
-  const expectedEdges = [
-    [0, 1],
-    [3, 0],
-    [2, 1],
-    [2, 3],
-    [4, 5],
-    [5, 6],
-    [6, 7],
-    [7, 4],
-    [0, 4],
-    [1, 5],
-    [2, 6],
-    [3, 7],
-  ];
-  expect(vertices).toEqual(expectedVertices);
-  expect(edges).toEqual(expectedEdges);
-});
+    const expectedVertices = [
+      [1, 3, 5],
+      [3, 3, 5],
+      [3, 5, 5],
+      [1, 5, 5],
+      [1, 3, 7],
+      [3, 3, 7],
+      [3, 5, 7],
+      [1, 5, 7],
+    ];
+    const expectedEdges = [
+      [0, 1],
+      [3, 0],
+      [2, 1],
+      [2, 3],
+      [4, 5],
+      [5, 6],
+      [6, 7],
+      [7, 4],
+      [0, 4],
+      [1, 5],
+      [2, 6],
+      [3, 7],
+    ];
+    expect(vertices).toEqual(expectedVertices);
+    expect(edges).toEqual(expectedEdges);
+  });
 
-test("make sure a cube with a size has the right vertices / edges", () => {
-  const vertices: Vertex[] = [];
-  const edges: Edge[] = [];
-  getCubeGeometry(vertices, edges, 0.1, 0, 0, 0);
+  test("make sure a cube with a size has the right vertices / edges", () => {
+    const vertices: Vertex[] = [];
+    const edges: Edge[] = [];
+    getCubeGeometry(vertices, edges, 0.1, 0, 0, 0);
 
-  const expectedVertices = [
-    [-0.1, -0.1, -0.1],
-    [0.1, -0.1, -0.1],
-    [0.1, 0.1, -0.1],
-    [-0.1, 0.1, -0.1],
-    [-0.1, -0.1, 0.1],
-    [0.1, -0.1, 0.1],
-    [0.1, 0.1, 0.1],
-    [-0.1, 0.1, 0.1],
-  ];
-  // prettier-ignore
-  const expectedEdges = [
+    const expectedVertices = [
+      [-0.1, -0.1, -0.1],
+      [0.1, -0.1, -0.1],
+      [0.1, 0.1, -0.1],
+      [-0.1, 0.1, -0.1],
+      [-0.1, -0.1, 0.1],
+      [0.1, -0.1, 0.1],
+      [0.1, 0.1, 0.1],
+      [-0.1, 0.1, 0.1],
+    ];
+    // prettier-ignore
+    const expectedEdges = [
     [0, 1],  [3, 0],   [2, 1],   [2, 3], // Top face
     [4, 5],  [5, 6],   [6, 7],   [7, 4], // Bottom face
     [0, 4],  [1, 5],   [2, 6],   [3, 7], // Sides
   ];
-  expect(vertices).toEqual(expectedVertices);
-  expect(edges).toEqual(expectedEdges);
+    expect(vertices).toEqual(expectedVertices);
+    expect(edges).toEqual(expectedEdges);
+  });
 });
