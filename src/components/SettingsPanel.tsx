ddf87a387573e4585418bc22eacbfa9096ea2cd0
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2024-06-06 10:50:37 +0200

    Change settings limits

--- src/components/SettingsPanel.tsx
+++ src/components/SettingsPanel.tsx
@@ -44,7 +44,7 @@ const SettingsPanel = ({ camera }: Props) => {
       <RangeSetting
         name="positionY"
         min={-1.0}
-        max={10.0}
+        max={20.0}
         step={0.1}
         type={"float"}
       />
